8b4a01d83a82fb2a47093323f42856717495e7ca
Krishna Verma <krishna.verma@deliveryhero.in>
2023-10-14 00:06:19 +0530

    First problem on codekris

---
+++ arrays/python/contains_duplicate.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/contains-duplicate/description/
+"""
+Check if the given list of integers contains any duplicates.
+    Parameters:
+        nums (List[int]): A list of integers.
+    Returns:
+        bool: True if the list contains duplicates, False otherwise.
+"""
+class Solution: 
+    def containsDuplicate(self, nums: List[int]) -> bool :
+        hashSet = set()
+        for num in nums :
+            if num in hashSet :
+                return True
+            hashSet.add(num)
+        return False
